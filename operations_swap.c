a55cc3ff3e573f1b861ab5d4a71c19c95370c185
Daniel Jiménez <danielji@student.42madrid.com>
2025-05-22 10:55:46 +0200

    operations_swap

--- operations_swap.c
+++ operations_swap.c
@@ -1,14 +1,31 @@
 /******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   operations.c                                       :+:      :+:    :+:   */
+/*   operations_swap.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 09:42:01 by danielji          #+#    #+#             */
-/*   Updated: 2025/05/22 09:42:52 by danielji         ###   ########.fr       */
+/*   Updated: 2025/05/22 10:05:53 by danielji         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
-#include "push_swap.h"
+/* #include "push_swap.h"
 
+// Swap the first 2 elements at the top of stack `a`
+void	sa(t_list **stack)
+{
+	ft_swap();
+}
+
+// Swap the first 2 elements at the top of stack `b`
+void	sb()
+{
+	
+}
+
+// `sa` and `sb` at the same time
+void	ss()
+{
+	
+} */
