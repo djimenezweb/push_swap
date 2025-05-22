3d01e859858456d053f6717d98babb973b4889f7
Daniel Jiménez <danielji@student.42madrid.com>
2025-05-22 11:39:18 +0200

    free array of strings

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 11:02:54 by danielji          #+#    #+#             */
-/*   Updated: 2025/05/21 17:06:21 by danielji         ###   ########.fr       */
+/*   Updated: 2025/05/22 11:38:37 by danielji         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -49,28 +49,48 @@ t_list	*initialize(char **arr)
 	return (list);
 }
 
+void	free_array_of_strings(char **arr)
+{
+	int	i;
+
+	i = 0;
+	while(arr[i])
+	{
+		free(arr[i]);
+		i++;
+	}
+	free(arr);
+}
+
 int	main(int argc, char **argv)
 {
 	t_list	*a_stack;
 	t_list	*b_stack;
+	int		needs_free;
 
+	needs_free = 0;
 	if (argc == 1)
 		return (0);
 	if (argc == 2 && ft_strchr(argv[1], ' '))
+	{
 		argv = ft_split(argv[1], ' ');
+		needs_free = 1;
+	}
 	else
 		argv = argv + 1;
 	validate(argv);
 	a_stack = initialize(argv);
 	b_stack = NULL;
-	print_list("Original:", a_stack);
-	/*ft_swap(&a_stack);
+	if (needs_free)
+		free_array_of_strings(argv);
+	/*print_list("Original:", a_stack);
+	ft_swap(&a_stack);
  	print_list("Swap:", a_stack);
 	ft_push(&a_stack, &b_stack);
 	print_list("Push (a):", a_stack);
-	print_list("Push (b):", b_stack);*/
+	print_list("Push (b):", b_stack);
 	ft_rrotate(&a_stack);
-	print_list("Rotate (a):", a_stack);
+	print_list("Rotate (a):", a_stack);*/
 	ft_lstclear(&a_stack, free);
 	ft_lstclear(&b_stack, free);
 	return (0);
