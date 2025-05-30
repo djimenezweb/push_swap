3313e5f775956af25994ff48a0a176626d489757
Daniel Jiménez <danielji@student.42madrid.com>
2025-05-30 14:46:49 +0200

    Combined files

--- algorithms/basic_sort.c
+++ algorithms/basic_sort.c
@@ -6,7 +6,7 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 10:02:56 by danielji          #+#    #+#             */
-/*   Updated: 2025/05/30 14:39:39 by danielji         ###   ########.fr       */
+/*   Updated: 2025/05/30 14:42:56 by danielji         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -27,6 +27,30 @@ t_list	*get_smallest(t_list **a)
 		temp = temp->next;
 	}
 	return (smallest);
+}
+
+// Sort three numbers in ascending order.
+void	sort_three(t_list **a, t_list **b)
+{
+	int	x;
+	int	y;
+	int	z;
+
+	x = get_content(*a);
+	y = get_content((*a)->next);
+	z = get_content((*a)->next->next);
+	if ((x < y) && (y > z) && (z < x)) // 231
+		rra(a, b);
+	else if ((x > y) && (y < z) && (z < x)) // 312
+		ra(a, b);
+	else
+	{
+		sa(a, b); //213
+		if ((x < y) && (x < z)) // 132
+			ra(a, b);
+		if ((x > y) && (x > z)) // 321
+			rra(a, b);
+	}
 }
 
 // Sort four or five numbers in ascending order.
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 12:24:12 by danielji          #+#    #+#             */
-/*   Updated: 2025/05/30 14:36:49 by danielji         ###   ########.fr       */
+/*   Updated: 2025/05/30 14:44:04 by danielji         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -22,6 +22,7 @@
 t_list	*initialize(char **arr);
 
 // Algorithms
+t_list	*get_smallest(t_list **a);
 void	sort_three(t_list **a, t_list **b);
 void	sort_four_or_five(t_list **a, t_list **b, int size);
 
