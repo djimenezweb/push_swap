1eea86e5215d723e678fe71e54db1b0aa1e3b889
Daniel Jiménez <danielji@student.42madrid.com>
2025-05-30 14:26:14 +0200

    optimized sort 5

--- algorithms/sort_five.c
+++ algorithms/sort_five.c
@@ -6,23 +6,14 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 10:02:56 by danielji          #+#    #+#             */
-/*   Updated: 2025/05/30 13:57:19 by danielji         ###   ########.fr       */
+/*   Updated: 2025/05/30 14:23:15 by danielji         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "../push_swap.h"
 
-static void push_by_value(t_list **a, t_list **b, int value)
-{
-	while (get_content(*a) != value)
-	{
-		ra(a, b);
-	}
-	pb(a, b);
-}
-
-// Sort four numbers in ascending order.
-void	sort_four(t_list **a, t_list **b)
+// Get smallest node.
+t_list	*get_smallest(t_list **a)
 {
 	t_list	*temp;
 	t_list	*smallest;
@@ -35,7 +26,18 @@ void	sort_four(t_list **a, t_list **b)
 			smallest = temp;
 		temp = temp->next;
 	}
-	push_by_value(a, b, get_content(smallest));
+	return (smallest);
+}
+
+// Sort four numbers in ascending order.
+void	sort_four(t_list **a, t_list **b)
+{
+	t_list	*smallest;
+
+	smallest = get_smallest(a);
+	while (get_content(*a) != get_content(smallest))
+		ra(a, b);
+	pb(a, b);
 	if (!is_sorted(*a))
 		sort_three(a, b);
 	pa(a, b);
@@ -44,31 +46,13 @@ void	sort_four(t_list **a, t_list **b)
 // Sort five numbers in ascending order.
 void	sort_five(t_list **a, t_list **b)
 {
-	int		value;
-	t_list	*temp;
 	t_list	*smallest;
-	t_list	*second_smallest;
 
-	temp = *a;
-	smallest = NULL;
-	second_smallest = NULL;
-	while (temp)
-	{
-		value = get_content(temp);
-		if (!smallest || value < get_content(smallest))
-		{
-			second_smallest = smallest;
-			smallest = temp;
-		}
-		else if (!second_smallest || value < get_content(second_smallest))
-			second_smallest = temp;
-		temp = temp->next;
-	}
-	ft_printf("-> Smallest: %i, %i\n", get_content(smallest), get_content(second_smallest));
-	push_by_value(a, b, get_content(smallest));
-	push_by_value(a, b, get_content(second_smallest));
+	smallest = get_smallest(a);
+	while (get_content(*a) != get_content(smallest))
+		ra(a, b);
+	pb(a, b);
 	if (!is_sorted(*a))
-		sort_three(a, b);
-	pa(a, b);
+		sort_four(a, b);
 	pa(a, b);
 }
