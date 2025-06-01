dcb650a570474ceba42ff4e90ce05a04dd346ac0
djimenezweb <djimenezweb@gmail.com>
2025-06-01 23:46:44 +0200

    Auxiliary function to split arguments. Check Valgrind

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: danielji <danielji@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 11:02:54 by danielji          #+#    #+#             */
-/*   Updated: 2025/06/01 12:25:32 by danielji         ###   ########.fr       */
+/*   Updated: 2025/06/01 23:35:19 by danielji         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -35,6 +35,19 @@ t_list	*initialize(char **arr)
 	return (list);
 }
 
+// Split arguments
+char	**split_args(char **argv, int *needs_free)
+{
+	if (ft_strchr(argv[1], ' '))
+	{
+		argv = ft_split(argv[1], ' ');
+		*needs_free = 1;
+	}
+	else
+		argv = argv + 1;
+	return (argv);
+}
+
 // This is where all the magic happens.
 // Minimal validation (80): 100 random numbers in fewer than 700 operations.
 // Maximum validation: 500 random numbers in no more than 5500 operations.
@@ -49,34 +62,26 @@ int	main(int argc, char **argv)
 	needs_free = 0;
 	if (argc == 1)
 		return (0);
-	if (argc == 2 && ft_strchr(argv[1], ' '))
-	{
-		argv = ft_split(argv[1], ' ');
-		needs_free = 1;
-	}
-	else
-		argv = argv + 1;
+	argv = split_args(argv, &needs_free);
 	validate(argv);
 	a_stack = initialize(argv);
 	b_stack = NULL;
 	size = ft_lstsize(a_stack);
 	if (needs_free)
 		free_array_of_strings(argv);
-	// print_list("Stack A ---> ", a_stack);
-	// print_list("Stack B ---> ", b_stack);
+	/*print_list("Stack A ---> ", a_stack);
+	print_list("Stack B ---> ", b_stack);*/
 	while (!is_sorted(a_stack))
 	{
 		if (size == 2)
 			sa(&a_stack, &b_stack);
-		else if (size == 3)
-			sort_three(&a_stack, &b_stack);
 		else if (size <= 5)
-			sort_four_or_five(&a_stack, &b_stack, size);
+			basic_sort(&a_stack, &b_stack);
 		else
 			ineffsort(&a_stack, &b_stack);
 	}
 	print_list("Stack A ---> ", a_stack);
-	// print_list("Stack B ---> ", b_stack);
+	//print_list("Stack B ---> ", b_stack);
 	ft_lstclear(&a_stack, free);
 	ft_lstclear(&b_stack, free);
 	return (0);
